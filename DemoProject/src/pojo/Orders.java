69dd48d36ed4ea07dfdcc54606de9f6b701641bd
Mariana Vives islas <1000075178@hexaware.com>
2022-12-08 10:27:36 -0600

    ADD changes to REST API project

---
+++ DemoProject/src/pojo/Orders.java
@@ -0,0 +1,22 @@
+package pojo;
+
+public class Orders {
+
+	private String couseTitle;
+	private String price;
+
+	public String getCouseTitle() {
+		return couseTitle;
+	}
+	public void setCouseTitle(String couseTitle) {
+		this.couseTitle = couseTitle;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	
+}
