948b60e3764e4dd91994b007e45cfb7994b880a2
Mariana Vives islas <1000075178@hexaware.com>
2022-12-16 15:18:09 -0600

    ADD cucumer reporting

--- APIFramework/src/test/java/cucumber/Options/TestRunner.java
+++ APIFramework/src/test/java/cucumber/Options/TestRunner.java
@@ -8,6 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/java/features", 
 glue= {"stepDefinations"},
+plugin = "json:target/jsonReports/cucumber-report.json",
 tags= {"@DeletePlace"})
 public class TestRunner {
 
